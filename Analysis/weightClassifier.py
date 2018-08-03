fedb026e9c1b428998d08cc22f8d848c25ae47e8
Mitchell Hole <mitchellhole@Mitchells-MBP.hitronhub.home>
2018-08-03 10:44:30 -0700

    bug

--- Analysis/weightClassifier.py
+++ Analysis/weightClassifier.py
@@ -32,5 +32,6 @@ model = make_pipeline(
 
 X = fighters[['height', 'reach', 'age']].values
 y = fighters['weight_class'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 model.fit(X_train, y_train)
-print("Accuracy: " + str(model.score(X_test, y_test)))
+print("Accuracy: " + str(round(model.score(X_test, y_test), 3)))
